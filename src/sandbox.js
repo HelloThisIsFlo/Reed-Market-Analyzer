fb85061b036e1aad2c038a2e68b7f49bde09ec72
Florian Kempenich <Flori@nKempenich.com>
2020-01-24 16:37:29 +0000

    Add cache support

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,6 +1,9 @@
 import dotenv from "dotenv";
 dotenv.config();
 import axios from "axios";
+import hash from "object-hash";
+import { dirname } from "path";
+import fs from "fs";
 
 import querystring from "querystring";
 
@@ -13,11 +16,55 @@ export const removeAllNulls = obj => {
     }, {});
 };
 
-class ReedApi {
+const storeData = (filename, data) => {
+  try {
+    fs.writeFileSync(filename, JSON.stringify(data));
+  } catch (err) {
+    console.error(err);
+  }
+};
+const loadData = filename => {
+  try {
+    return JSON.parse(fs.readFileSync(filename, "utf8"));
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
+};
+
+class Cache {
   constructor() {
+    this.path = dirname(dirname(require.main.filename)) + "/.cache";
+  }
+
+  has(params) {
+    return fs.existsSync(this.filename(params));
+  }
+
+  save(params, result) {
+    storeData(this.filename(params), result);
+  }
+
+  get(params) {
+    return loadData(this.filename(params));
+  }
+
+  delete(params) {
+    fs.unlinkSync(this.filename(params));
+  }
+
+  filename(params) {
+    const paramsHash = hash(params);
+    return `${this.path}/${paramsHash}.json`;
+  }
+}
+
+class ReedApi {
+  constructor(cache) {
     const reedApiKey = process.env.REED_API_KEY;
     const reedApiVersion = "1.0";
 
+    this.cache = cache;
     this.axiosReedApi = axios.create({
       baseURL: `https://www.reed.co.uk/api/${reedApiVersion}`,
       auth: {
@@ -62,14 +109,14 @@ class ReedApi {
     });
     const fetchPage = async alreadyFetched => {
       const resultsToTake = 100;
-      const resp = await this.axiosReedApi.get(
+      const resp = await this.getWithCache(
         `/search?${querystring.stringify({
           ...searchParams,
           resultsToTake,
           resultsToSkip: alreadyFetched
         })}`
       );
-      const { results, totalResults } = resp.data;
+      const { results, totalResults } = resp;
       return { results, totalResults };
     };
 
@@ -89,9 +136,17 @@ class ReedApi {
     return allResults;
   }
 
-  async details(jobId) {
-    const resp = await this.axiosReedApi.get(`/jobs/${jobId}`);
+  async getWithCache(url, forceRefresh = false) {
+    if (forceRefresh) this.cache.delete(url);
+    if (this.cache.has(url)) return this.cache.get(url);
+
+    const resp = await this.axiosReedApi.get(url);
+    this.cache.save(url, resp.data);
     return resp.data;
+  }
+
+  async details(jobId) {
+    return this.getWithCache(`/jobs/${jobId}`);
   }
 
   async detailsForAll(searchResults) {
@@ -102,16 +157,24 @@ class ReedApi {
 }
 
 async function main() {
-  const reedApi = new ReedApi();
-  const results = await reedApi.search({ keywords: "python" });
-  console.log("results.length", results.length);
+  const demoApi = async () => {
+    const cache = new Cache();
+    const reedApi = new ReedApi(cache);
+    const results = await reedApi.search({ keywords: "python" });
+    console.log("results.length", results.length);
 
-  const detailRes = await reedApi.details(39680634);
-  console.log(detailRes);
+    const detailRes = await reedApi.details(39680634);
+    console.log(detailRes);
 
-  const jobDetails = await reedApi.detailsForAll(results);
-  console.log(jobDetails.length);
-  console.log(jobDetails[99]);
+    const jobDetails = await reedApi.detailsForAll(results);
+    console.log(jobDetails.length);
+    console.log(jobDetails[99]);
+  };
+  const demoCache = async () => {
+    const cache = new Cache();
+    console.log(cache.has({ this: "is", a: 4, test: true }));
+  };
+  await demoApi();
 }
 
 main().catch(e => console.log(e));
